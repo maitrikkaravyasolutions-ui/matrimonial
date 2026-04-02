71bd94d87ad2023aa3422b2d490f4436b14df8f4
Chintan Mirani <chintan.mirani@gmail.com>
2026-04-02 11:10:29 +0530

    responsive content

--- resources/views/layouts/common_content.blade.php
+++ resources/views/layouts/common_content.blade.php
@@ -4,6 +4,8 @@
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Matrimonial | Dashboard</title>
 
